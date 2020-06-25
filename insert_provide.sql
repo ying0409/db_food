3852ea687bb988b7137d72d3ed791eb899a2f7dd
Drena26 <33838481+Drena26@users.noreply.github.com>
2020-06-25 15:50:51 +0800

    Add files via upload

---
+++ insert_provide.sql
@@ -0,0 +1,8 @@
+INSERT INTO provide (`s_id` , `d_id`, `t_id`) VALUES
+('4' ,'1','0606');
+
+INSERT INTO provide (`s_id` , `d_id`, `t_id`) VALUES
+('5' ,'2','0607');
+
+INSERT INTO provide (`s_id` , `d_id`, `t_id`) VALUES
+('4' ,'3','0608');
