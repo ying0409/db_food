a6498353a9f35c431de28519c3acfc6947c69522
Drena26 <33838481+Drena26@users.noreply.github.com>
2020-06-25 15:51:29 +0800

    Add files via upload

---
+++ insert_comment.sql
@@ -0,0 +1,8 @@
+INSERT INTO comment (`c_id`, `s_id`, `star`, `content`) VALUES
+('1', '4', '5','cheap!');
+
+INSERT INTO comment (`c_id`, `s_id`, `star`, `content`) VALUES
+('2', '5', '4.5','yummy!');
+
+INSERT INTO comment (`c_id`,`s_id`, `star`, `content`) VALUES
+('3', '6', '4','healthy!');
---
+++ insert_discount.sql
@@ -0,0 +1,8 @@
+INSERT INTO discount ( `d_id`, `content`) VALUES
+('1' ,'20_OFF');
+
+INSERT INTO discount ( `d_id`, `content`) VALUES
+('2' ,'FREE !!');
+
+INSERT INTO discount ( `d_id`, `content`) VALUES
+('3' ,'buy one get one free');
