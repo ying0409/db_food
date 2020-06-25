c1c677e07a6553641297d5b4c50d98bba514df76
ying0409 <65957197+ying0409@users.noreply.github.com>
2020-06-25 15:38:50 +0800

    Add files via upload

--- food.sql
+++ food.sql
@@ -18,15 +18,15 @@ create table manager(
 
 create table shop(
 	s_id		varchar(5),
-	m_id      varchar(5),
+	m_id		varchar(5),
 	name		varchar(10) not null,
-	time_id   varchar(5),
-	menu_id	  varchar(5),
-	style 	varchar(10),
-	phone 	varchar(10),
-	website   varchar(20),
-	address   varchar(20),
-	aveprice  numeric(4,0),
+	time_id		varchar(5),
+	menu_id		varchar(5),
+	style		varchar(10),
+	phone		varchar(10),
+	website		varchar(30),
+	address		varchar(30),
+	aveprice	numeric(4,0),
 	primary key(s_id),
 	foreign key (m_id) references manager(m_id)
 		on delete set null
@@ -34,27 +34,19 @@ create table shop(
 
 create table discount(
 	d_id		varchar(5),
-	content	varchar(10) not null,
-	date date,
+	content		varchar(30) not null,
 	primary key(d_id)
 ) ENGINE=INNODB;
 
 create table provide(
 	s_id		varchar(5),
 	d_id		varchar(5),
-	t_id            varchar(5),
+	t_id        date,
 	primary key(s_id,d_id),
 	foreign key (s_id) references shop(s_id)
 		on delete cascade,
 	foreign key (d_id) references discount(d_id)
 		on delete cascade
-) ENGINE=INNODB;
-
-create table menu(
-	menu_id		varchar(5),
-	name		varchar(10),
-	price     numeric(4,0),
-	primary key(menu_id,name)
 ) ENGINE=INNODB;
 
 create table favorate(
@@ -71,7 +63,7 @@ create table comment(
 	c_id		varchar(5),
 	s_id		varchar(5),
 	star      	numeric(5,0) not null,
-	content   	varchar(10),
+	content   	varchar(30),
 	primary key(c_id,s_id),
 	foreign key (c_id) references customer(c_id)
 		on delete cascade,
@@ -80,11 +72,22 @@ create table comment(
 ) ENGINE=INNODB;
 
 create table time(
-	time_id 	varchar(5),
+	s_id 		varchar(5),
 	day 		varchar(10),
 	op_hr 		numeric(2) check (op_hr >= 0 and op_hr < 24),
 	op_min 		numeric(2) check (op_min >= 0 and op_min < 60),
 	cl_hr 		numeric(2) check (cl_hr >= 0 and cl_hr < 24),
 	cl_min 		numeric(2) check (cl_min >= 0 and cl_min < 60),
-	primary key(time_id,day,op_hr,op_min,cl_hr,cl_min)
+	primary key(s_id,day,op_hr,op_min,cl_hr,cl_min),
+	foreign key (s_id) references shop(s_id)
+		on delete cascade
+) ENGINE=INNODB;
+
+create table menu(
+	s_id 		varchar(5),
+	name		varchar(10),
+	price     	numeric(4,0),
+	primary key(s_id,name),
+	foreign key (s_id) references shop(s_id)
+		on delete cascade
 ) ENGINE=INNODB;
