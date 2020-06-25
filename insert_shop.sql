d27f8aa9994168a73eb28b69471535f458a47519
ying0409 <65957197+ying0409@users.noreply.github.com>
2020-06-25 16:24:03 +0800

    Add files via upload

--- insert_shop.sql
+++ insert_shop.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO shop (`s_id` , `m_id`, `name`, `time_id`,`menu_id`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
-('4' ,'1', 'hotpotIN', '7','4','hotpot','0223670672','https://reurl.cc/5ly93z' ,'Taipei', '160');
+INSERT INTO shop (`s_id` , `m_id`, `name`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
+('4' ,'1', 'hotpotIN','hotpot','0223670672','https://reurl.cc/5ly93z' ,'Taipei', '160');
 
-INSERT INTO shop (`s_id` , `m_id`, `name`, `time_id`,`menu_id`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
-('5' ,'2', 'hotpotduck', '8','5','hotpot','0223626016','https://reurl.cc/qdjW4R' ,'Taipei', '170');
+INSERT INTO shop (`s_id` , `m_id`, `name`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
+('5' ,'2', 'hotpotduck','hotpot','0223626016','https://reurl.cc/qdjW4R' ,'Taipei', '170');
 
 
-INSERT INTO shop (`s_id` , `m_id`, `name`, `time_id`,`menu_id`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
-('6' ,'3', '12mini', '9','6','hotpot','0222222222','https:QQQ' ,'taipei', '200');
+INSERT INTO shop (`s_id` , `m_id`, `name`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
+('6' ,'3', '12mini','hotpot','0222222222','https:QQQ' ,'taipei', '200');
 
-INSERT INTO shop (`s_id` , `m_id`, `name`, `time_id`,`menu_id`, `style`, `phone`, `website`, `address`, `aveprice`) VALUES
-('7' ,'2', '50drink', '10','7','drink','0211111111','https:XDXD' ,'taipei', '60');
+INSERT INTO shop (`s_id` , `m_id`, `name`,  `style`, `phone`, `website`, `address`, `aveprice`) VALUES
+('7' ,'2', '50drink','drink','0211111111','https:XDXD' ,'taipei', '60');
 
