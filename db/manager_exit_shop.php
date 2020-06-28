3401c9036605a862be09a13734113d0f2dd3d14f
shin-han-7 <55297761+shin-han-7@users.noreply.github.com>
2020-06-28 11:48:06 +0800

    Add files via upload

--- db/manager_exit_shop.php
+++ db/manager_exit_shop.php
@@ -21,14 +21,6 @@
 			-webkit-border-radius: 5px;
 			border-radius: 5px;
 		}
-
-		input[type="text"] {
-			padding: 5px 150px;
-			border: 2px black solid;
-			cursor: pointer;
-			-webkit-border-radius: 5px;
-			border-radius: 5px;
-		}
 	</style>
 	<?php
         $servername = "localhost";
@@ -44,7 +36,7 @@
             die("Connection failed: " . $conn->connect_error);
         } 
         /**************************/
-        $S_ID='4';
+        $S_ID='5';
         $shop_query ="SELECT * FROM shop WHERE s_id='$S_ID'";
         $shop_result = $conn->query($shop_query);
         if ($shop_result->num_rows > 0) 
@@ -55,25 +47,34 @@
             $phone_value=$row[phone];
             $address_value=$row[address];
         }
-       //
+        //ex:name='menu_name_$count."'
         $menu_query ="SELECT * FROM menu WHERE s_id='$S_ID'";
         $menu_result = $conn->query($menu_query);
         if ($menu_result->num_rows > 0) 
         {
+            $menu_value='<tr><td>Name</td><td>Price</td><tr>';
+            //$count=1;
             while($row = mysqli_fetch_array ( $menu_result, MYSQLI_ASSOC ) ) {
-                $menu_value=$menu_value.'/ '.$row[d_name].':'.$row[price];
-            }
+                //$menu_value=$menu_value.'/ '.$row[d_name].':'.$row[price];
+                $menu_value=$menu_value."<tr><td>".$row[d_name]."</td><td>";//<input type='text' value='".$row[d_name]."' name='menu_name_".$count."'/></td>";
+                $menu_value=$menu_value."<td>".$row[price]."</td></tr>";//<td><input type='text' value='".$row[price]."' name='menu_price_".$count."'/></td></tr>";
+                //$count=$count+1;
+            }/**/
         }
-        //
+        //ex:name='time-op_hr-1'
         $time_query ="SELECT * FROM time WHERE s_id='$S_ID'";
         $time_result = $conn->query($time_query);
         if ($time_result->num_rows > 0) 
         {
             while($row = mysqli_fetch_array ( $time_result, MYSQLI_ASSOC ) ) {
-                $time_value=$time_value.'/ '.$row[day].':'.$row[op_hr].':'.$row[op_min].'~'.$row[cl_hr].':'.$row[cl_min];
+                $time_value=$time_value."<tr><td>星期".$row[day].":open</td>";
+                $time_value=$time_value."<td><input type='text' value='".$row[op_hr]."' name='time-op_hr-".$row[day]."'/></td><td>:</td>";
+                $time_value=$time_value."<td><input type='text' value='".$row[op_min]."' name='time-op_min-".$row[day]."'/></td><td>~</td>";
+                $time_value=$time_value."<td><input type='text' value='".$row[cl_hr]."' name='time-cl_hr-".$row[day]."'/></td><td>:</td>";
+                $time_value=$time_value."<td><input type='text' value='".$row[cl_min]."' name='time-cl_min-".$row[day]."'/></td></tr>";
             }
         }
-        //
+        //discount_1
         $discount_query ="SELECT * FROM discount WHERE d_id in (
             SELECT D.d_id
             FROM provide as P natural join discount as D
@@ -83,13 +84,13 @@
         if ($discount_result->num_rows > 0) 
         {
             while($row = mysqli_fetch_array ( $discount_result, MYSQLI_ASSOC ) ) {
-                $discount_value=$discount_value.'/ '.$row[content];
+                //$discount_value=$discount_value.'/ '.$row[content];
+                $discount_value=$discount_value."<tr><td>[ID:".$row[d_id]."]</td><td>Content:".$row[content]."</td></tr>";
             }
         }
              
 
     ?>
-
 	<script type="text/javascript">
 	    function init()
 		{
@@ -112,15 +113,18 @@
 
             //menu
             var menu_value="<?php echo $menu_value; ?>";
-            document.getElementById("menu").value=menu_value;
+            //alert(menu_value);
+            document.getElementById("menu").innerHTML = menu_value;
+
 
             //time
             var time_value="<?php echo $time_value; ?>";
-            document.getElementById("time").value=time_value;
+            //alert(time_value);
+            document.getElementById("time").innerHTML = time_value;
 
             //discount
             var discount_value="<?php echo $discount_value; ?>";
-            document.getElementById("discount").value=discount_value;
+            document.getElementById("discount").innerHTML =discount_value;
 		}
 	</script>
 </head>
@@ -133,42 +137,63 @@
         <h2 align='center'><img style="margin-top: 15px" src="FOOD.gif" /></h2>
 
         <form action="manager_update.php" method="post">
-            <h2 align='center' style="margin-top: 25px">基本資料</h2>
-            <h3 align='center'>
-                <th>Website</th>
-                <input type="text"  id="website" name="website" />
-            </h3>
-            <h3 align='center'>
-                <th>style</th>
-                <input type="text" id="style" name="style" />
-            </h3>
-            <h3 align='center'>
-                <th>Phone</th>
-                <input type="text" id="phone" name="phone" />
-            </h3>
-            <h3 align='center'>
-                <th>Address</th>
-                <input type="text" id="address" name="address" />
-            </h3>
+            <div style="margin:0px auto;width:80%;">
+                <h2 align="center" style="margin-top: 25px">基本資料</h2>
+                <table  align="center">
+                    <tr>
+                        <td>Website</td>
+                        <td><input type="text" class="basis_input" id="website" name="website" /></td>
+                    </tr>
+                    <tr>
+                        <td>style</td>
+                        <td><input type="text" class="basis_input" id="style" name="style" /></td>
+                    </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td><input type="text" class="basis_input" id="phone" name="phone" /></td>
+                    </tr>
+                    <tr">
+                        <td>Address</td>
+                        <td><input type="text" class="basis_input" id="address" name="address" /></td>
+                    </tr>
+                </table>
+            </div>
 
             <hr>
             <h2 align='center' style="margin-top: 25px">菜單</h2>
-            <h3 align='center'>
-                <input type="text" id="menu" name="menu" />
-            </h3>
+            <table id="menu" align='center'></table>
+            <table align='center' style="margin-top: 15px">
+                <tr>
+                    <td>新增一道菜=></td>
+                    </td>
+                    <td>Name:<input type="text" name="add_food_name" /></td><td> Price:<input type="text" name="add_food_price" /></td> 
+                </tr>
+                <tr>
+                    <td>刪除一道菜=></td>
+                    <td>Name:<input type="text" name="delete_food_name" /></td><td> </td>
+                </tr>
+            </table>
 
             <hr>
             <h2 align='center' style="margin-top: 25px">營業時間</h2>
-            <h3 align='center'>
-                <input type="text" id="time" name="time" />
-            </h3>
+            <table id="time" align='center'></table>
+            
 
             <hr>
             <h2 align='center' style="margin-top: 25px">優惠活動</h2>
-            <h3 align='center'>
-                <input type="text" id="discount" name="discount" />
-            </h3>
-
+            <table id="discount" align='center'></table>
+            <table align='center' style="margin-top: 15px">
+                <tr>
+                    <td>新增優惠活動=></td>
+                    <td>輸入活動內容：</td>
+                    <td><input type="text" name="add_discount" /></td> 
+                </tr>
+                <tr>
+                    <td>刪除優惠活動=></td>
+                    <td>輸入活動ID:</td>
+                    <td><input type="text" name="delete_d_id" /></td>
+                </tr>
+            </table>
 
             <h3 align='center'>
                 <input type="submit" value="更新" />
--- db/manager_update.php
+++ db/manager_update.php
@@ -19,39 +19,139 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$_input = 4;
+$_input = 5;
+$_correct=TRUE;
 
-if (isset($_POST['website']) &&  isset($_POST['style']) && isset($_POST['phone']) && isset($_POST['address']) && isset($_POST['menu']) && isset($_POST['time']) && isset($_POST['discount'])) {
+if (isset($_POST['website']) &&  isset($_POST['style']) && isset($_POST['phone']) && isset($_POST['address'])) {
     //$photo = $_POST['photo']
     $website = $_POST['website'];
     $style = $_POST['style'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
-    $menu = $_POST['menu'];
-    $time = $_POST['time'];
-    $discount = $_POST['discount'];
-
-
     $update_shop="UPDATE shop SET website='$website',style='$style',phone='$phone',address='$address' WHERE s_id = '$_input'";
-    //$update_time="UPDATE time SET time=$time WHERE s_id = $_input";
-    //$update_menu="UPDATE menu SET menu=$menu WhERE s_id = $_input";
-    /*$update_discount="UPDATE discount SET content='$discount' WHERE d_id in (
-        SELECT D.d_id
-        FROM shop as S natural join provide as P natural join discount as D
-        WHERE 
-    )";*/
-
-	if ($conn->query($update_shop) === TRUE) {
-        echo "<div align='center'><h3>已更新!</h3>
-		<a href='manager_shop.php'>返回</a></div>";
-    } 
-    else {
+    if ($conn->query($update_shop) === FALSE) {
+        echo "error:update shop";
         echo "<h2 align='center'><font color='antiquewith'>更新失敗!!</font></h2><br>
         <div align='center'>
         <a href='manager_exit_shop.php'><再試一次></a>
         <a href='manager_shop.php'><返回></a>
         </div>";
-	}
+        $_correct=FALSE;
+    } 
+
+    //time
+    $query = "SELECT * FROM time where s_id = '$_input'";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        while($row = mysqli_fetch_array ( $result, MYSQLI_ASSOC ) ) {
+            $op_hr=$_POST["time-op_hr-$row[day]"];
+            $op_min=$_POST["time-op_min-$row[day]"];
+            $cl_hr=$_POST["time-cl_hr-$row[day]"];
+            $cl_min=$_POST["time-cl_min-$row[day]"];
+            $update_time="UPDATE time SET op_hr='$op_hr',op_min='$op_min',cl_hr='$cl_hr',cl_min='$cl_min'  WHERE day = '$row[day]' and s_id='$_input' ";
+            if ($conn->query($update_time) === FALSE){
+                echo 'error:time update';
+                echo "<h2 align='center'><font color='antiquewith'>更新失敗!!</font></h2><br>
+                <div align='center'>
+                <a href='manager_exit_shop.php'><再試一次></a>
+                <a href='manager_shop.php'><返回></a>
+                </div>";
+                $_correct=FALSE;
+            }
+        }
+    }
+    
+    //menu
+    if (isset($_POST['add_food_name']) &&  isset($_POST['add_food_price']))
+    {
+        $add_name=$_POST['add_food_name'];
+        $add_price=$_POST['add_food_price'];
+        $insert_menu="INSERT into menu(s_id,d_name,price) values('$_input','$add_name','$add_price') ";
+        if ($conn->query($insert_menu) === FALSE){//error
+            echo 'no insert menu';
+            $_correct=FALSE;
+        }
+    }else{
+        echo "data lost:add menu";
+        $_correct=FALSE;
+    }
+    
+    if(isset($_POST['delete_food_name']))
+    {
+        $delete_name=$_POST['delete_food_name'];
+        $delete_menu="DELETE FROM menu where d_name='$delete_name' and s_id='$_input'"; 
+        if ($conn->query($delete_menu) === FALSE){
+            echo 'no delete menu';
+            $_correct=FALSE;
+        }
+    }else{
+        echo "data lost:delete menu";
+        $_correct=FALSE;
+    }
+    
+    //discount
+    if (isset($_POST['add_discount']))
+    {
+        $add_content=$_POST['add_discount'];
+        $add_d_id=0;
+        $found=FALSE;
+        $query_d = "SELECT * FROM discount ";
+        $result_d = $conn->query($query_d);
+        if ($result_d->num_rows > 0) {
+            while($found == FALSE)
+            {
+                $add_d_id=$add_d_id+1;
+                $found=TRUE;
+                while($row = mysqli_fetch_array ( $result_d, MYSQLI_ASSOC ) ) {
+                    if($add_d_id == $row[d_id]){
+                        $found=FALSE;
+                        break;
+                    }
+                }
+            }
+        }
+        $insert_discount="INSERT into discount values('$add_d_id','$add_content') ";
+        if ($conn->query($insert_discount) === TRUE){
+            $insert_provide="INSERT into provide('s_id','d_id','t_id') values('$_input','$add_d_id','00000000') ";//t_id not done
+            if($conn->query($insert_provide) === FALSE){
+                echo'no insert provide';
+                $_correct=FALSE;
+            }
+        }else{//error
+            echo 'no insert discount';
+            $_correct=FALSE;
+        }
+
+    }else{
+        echo "data lost:add discount";
+        $_correct=FALSE;
+    }
+    if(isset($_POST['delete_d_id']))
+    {
+        $delete_d_id=$_POST['delete_d_id'];
+        $delete_discount="DELETE FROM discount where d_id='$delete_d_id' "; 
+        if ($conn->query($delete_discount) === FALSE){
+            echo 'no delete discount';
+            $_correct=FALSE;
+        }
+    }else{
+        echo "data lost:delete discount";
+        $_correct=FALSE;
+    }
+    
+    if($_correct === TRUE)
+    {
+        echo "<div align='center'><h3>已更新!</h3>
+		<a href='manager_shop.php'>返回</a></div>";
+    }
+    else{
+        echo "<h2 align='center'><font color='antiquewith'>更新錯誤!!</font></h2><br>
+        <div align='center'>
+        <a href='manager_exit_shop.php'><再試一次></a>
+        <a href='manager_shop.php'><返回></a>
+        </div>";
+    }
+    
 }
 else{
 	echo "資料不完全";
