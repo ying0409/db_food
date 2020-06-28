83036500e55d67ac621937483d24cd81f4b6fa34
Drena26 <33838481+Drena26@users.noreply.github.com>
2020-06-28 16:57:00 +0800

    Add files via upload

--- db/comment.php
+++ db/comment.php
@@ -1,7 +1,15 @@
+<html>
+<head>
+	<title>評論</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body bgcolor="#FFFAFA">
+<h1 align="center" style="margin-top: 25px">評論</h1>
+<h2 align='center'><img style="margin-top: 5px" src="FOOD.gif" /></h2>
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "cindy88409";
+$password = "foodmap11";
 $dbname = "db_food";
 
 // Connecting to and selecting a MySQL database
@@ -17,18 +25,89 @@ if ($link->connect_error) {
     die("Connection failed: " . $link->connect_error);
 } 
 
-$_input = $_GET["num"];
+$s_id = $_GET['s_id'];
+$c_id = $_GET['c_id'];
 
-$query = "SELECT * FROM shop NATURAL JOIN comment where s_id=$_input";
+$is_exist=0;
+
+echo "<h3 align='center'><font color='antiquewith'><a href = 'customer_shop.php? num=$s_id&my_id=$c_id'>回到店家資訊</a></font></h3>";
+
+$query = "SELECT * FROM customer NATURAL JOIN shop NATURAL JOIN comment where s_id=$s_id";
+echo "<table align='center' width='300' border='1'>";
+echo "<tr><td>Name</td>";
+echo "<td>Star</td>";
+echo "<td>Commend</td></tr>";
 $result = $link->query($query);
 if ($result->num_rows > 0) {
 	while($row = mysqli_fetch_array ( $result, MYSQLI_ASSOC ) ) {
-		echo "Comment<br>";
-		echo "Star: ";
-		echo "$row[star] <br>";
-		echo "Comment: ";
-		echo "$row[content]   <br>";
+		echo "<tr><td>$row[c_name]</td>";
+		echo "<td>";
+		$is_exist=1;
+		switch ($row[star]) {
+			case 0:
+				echo "☆☆☆☆☆";
+				break;
+			case 1:
+				echo "★☆☆☆☆";
+				break;
+			case 2:
+				echo "★★☆☆☆";
+				break;
+			case 3:
+				echo "★★★☆☆";
+				break;
+			case 4:
+				echo "★★★★☆";
+				break;
+			case 5:
+				echo "★★★★★";
+				break;
+		}
+		echo "</td><td>$row[content]</td></tr>";
 	}
 }
+echo "</table><br><br>";
 
+if($is_exist===0)
+{
+	echo "<h2 align='center'><font color='antiquewith'>目前尚無評論QQ<br> </font></h2>";
+	echo "<h2 align='center'><font color='antiquewith'>★歡迎留下你的珍貴意見★★<br> </font></h2>";
+	echo "<h2 align='center'><font color='antiquewith'>你的意見可以讓店家更好!<br> </font></h2>";
+}
+
+// echo "$c_id<br>";
+// echo "$s_id<br>";
 ?>
+	<style>
+    input[type="submit"]{padding:5px 15px; background:#ccc; border:0 none;
+	cursor:pointer;
+	-webkit-border-radius: 5px;
+	border-radius: 5px; }
+	
+	input[type="button"]{padding:5px 15px; background:#ccc; border:0 none;
+	cursor:pointer;
+	-webkit-border-radius: 5px;
+	border-radius: 5px; }
+    
+    input[type="text"]{padding:5px 120px; border:2px black solid;
+	cursor:pointer;
+	-webkit-border-radius: 5px;
+	border-radius: 5px; }
+	
+	</style>
+	<h3 align="center">新增評論</h3>
+	<form action="add_comment.php" method="post">	
+	<input type="hidden" name="c_id" value="<?php echo $c_id; ?>"	/>
+	<input type="hidden" name="s_id" value="<?php echo $s_id; ?>"	/>
+	<h3 align='center'><th>星級：</th>
+	<input type="text" name="star"  />
+	
+	<h3 align='center'><th>評論：</th>
+	<input type="text" name="content" /><br><br>
+	<input type="submit" value="新增"/>
+
+	</form>
+	
+</body>
+	
+</html>
